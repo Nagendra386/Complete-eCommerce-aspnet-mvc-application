6cb354805818f41b6fe0d37ea62e79c0e13c376d
SHAIK SAIDAWALI <SHAIK SAIDAWALI@DESKTOP-9N8GH54>
2023-10-20 11:08:42 +0530

    Add project files.

---
+++ eTicket/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace eTicket
+{
+	public class StartUp
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllersWithViews();//services.AddMvc(); would also work still
+
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
+}
